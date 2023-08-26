b33e874481869f0712f7fd4af042e3be5d32ec83
cookienocreams <58923195+cookienocreams@users.noreply.github.com>
2023-08-25 20:46:11 -0700

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, Phaj, DocumenterTools
 makedocs(sitename="P.H.A.J. Documentation")
 
 deploydocs(
-    repo = "github.com/cookienocreams/Phaj"
+    repo = "github.com/cookienocreams/Phaj.git"
     , branch = "docs/"
     , devbranch = "main"
 )
